c54e972b952bb276bd91a983ca3dd41d61618fe9
BrianSungUSFCA <csung4@dons.usfca.edu>
2018-02-27 07:43:46 +0000

    Create find_max_s

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROGS = driver
 
-OBJS = sum_array_c.o sum_array_s.o
+OBJS = sum_array_c.o sum_array_s.o find_max_c.o find_max_s.o
 CFLAGS = -g
 
 all : ${PROGS}
@@ -10,6 +10,12 @@ sum_array_c.o : sum_array_c.c
 
 sum_array_s.o : sum_array_s.s
 	as -o sum_array_s.o sum_array_s.s
+
+find_max_c.o : find_max_c.c
+	gcc -c -o find_max_c.o find_max_c.c
+
+find_max_s.o: find_max_s.s
+	as -o find_max_s.o find_max_s.s
 
 driver : driver.c ${OBJS}
 	gcc -o driver driver.c ${OBJS}
--- driver.c
+++ driver.c
@@ -3,12 +3,16 @@
 
 int sum_array_c(int *, int);
 int sum_array_s(int *, int);
+int find_max_c(int *, int);
+int find_max_s(int *, int);
 
 void test_sum_array();
+void test_find_max();
 
 int main(int argc, char **argv) {
 
     test_sum_array();
+    test_find_max();
     
     return 0;
 }
@@ -45,4 +49,39 @@ void test_sum_array() {
     printf("sum_array_c(*large_set, 1001) = %d\n", sum);
     sum = sum_array_s(large_set, 1001);
     printf("sum_array_s(*large_set, 1001) = %d\n", sum);
+}
+
+void test_find_max() {
+    int positive[4] = {2, 4, 6, 8};
+    int negative[4] = {-2, -4, -6, -8};
+    int zero[5] = {0, -2, 0, 0, 0};
+    int large_set[1001];
+    int i, max;
+
+    for (i = 0; i < 1001; i++) {
+        large_set[i] = i * 2;
+    }
+
+    printf("\n");
+    printf("find_max:\n");
+
+    max = find_max_c(positive, 4);
+    printf("find_max_c({2, 4, 6, 8}, 4) = %d\n", max);
+    max = find_max_s(positive, 4);
+    printf("find_max_s({2, 4, 6, 8}, 4) = %d\n", max);
+    
+    max = find_max_c(negative, 4);
+    printf("find_max_c({-2, -4, -6, -8}, 4) = %d\n", max);
+    max = find_max_s(negative, 4);
+    printf("find_max_s({-2, -4, -6, -8}, 4) = %d\n", max);
+
+    max = find_max_c(zero, 5);
+    printf("find_max_c({0, -2, 0, 0, 0}, 5) = %d\n", max);
+    max = find_max_s(zero, 5);
+    printf("find_max_s({0, -2, 0, 0, 0}, 5) = %d\n", max);
+
+    max = find_max_c(large_set, 1001);
+    printf("find_max_c(*large_set, 1001) = %d\n", max);
+    max = find_max_s(large_set, 1001);
+    printf("find_max_s(*large_set, 1001) = %d\n", max);
 }
